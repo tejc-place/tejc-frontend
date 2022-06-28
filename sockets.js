4a82e60c5d2af8aa244ca1aa0f8fc23af392375a
goatbed <75466655+goatbed@users.noreply.github.com>
2022-06-28 12:46:23 -0700

    add basic safari support for sockets

--- sockets.js
+++ sockets.js
@@ -1,7 +1,20 @@
-const sw = new SharedWorker("/socket_worker.js");
-sw.port.addEventListener("message", (e) => {
-    if (e.data.n == "alert") {
-        alert(e.data.p[0]);
-    }
-});
-sw.port.start();
+if ("SharedWorker" in window) {
+    const sw = new SharedWorker("/socket_worker.js");
+    sw.port.addEventListener("message", (e) => {
+        if (e.data.n == "alert") {
+            alert(e.data.p[0]);
+        }
+    });
+    sw.port.start();
+} else {
+    var ws = new WebSocket(`wss://tejc.place:8352/`);
+    ws.addEventListener("message", (e) => {
+        if (e.data === "heartbeat") {
+            return;
+        }
+        let data = JSON.parse(e.data);
+        if (data.n === "alert") {
+            alert(data.p[0]);
+        }
+    });
+}
